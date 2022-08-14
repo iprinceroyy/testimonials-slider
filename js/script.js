8e57067599c9f9ce258fe282ce84c8bfd67a8d6f
Prince Roy <princeku.ray143@gmail.com>
2022-08-14 18:17:44 +0530

    update slider design

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const gotToSlide = currSlide => {
     // Add transform property on each slide
     slides.forEach(
         (slide, i) =>
-        (slide.style.transform = `translateX(${100 * (i - currSlide)}%)`)
+        (slide.style.transform = `translateX(${105 * (i - currSlide)}%)`)
     );
 };
 
